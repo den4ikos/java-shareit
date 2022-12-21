3077ca6ae86648c7e88f860be1235f152f7efb6a
den4ikos <denasius89@yandex.ru>
2022-12-21 21:17:05 +0300

    added entity

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -3,24 +3,31 @@ package ru.practicum.shareit.booking;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 import ru.practicum.shareit.booking.validation.EnumStatusTypePattern;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name = "bookings", schema = "public")
 public class Booking {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @Column(name = "date_start")
     private LocalDateTime start;
-
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @Column(name = "date_end")
     private LocalDateTime end;
-
-    private Long item;
-
-    private Long booker;
-
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "item_id")
+    private Item item;
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "booker_id")
+    private User booker;
     @EnumStatusTypePattern
     private StatusType status;
 
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -3,22 +3,28 @@ package ru.practicum.shareit.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
+@Entity
+@Table(name = "users", schema = "public")
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
     @NotBlank
+    @Column
     private String name;
 
     @NotNull
     @NotBlank
     @Email(message = "Please provide a valid email address")
+    @Column
     private String email;
 }
